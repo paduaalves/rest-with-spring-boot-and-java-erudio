6361682b9f77077e8c8d86104af3535b7fd0c623
padua <paduaalves03@gmail.com>
2023-03-18 15:12:56 -0300

    Implementação de subtração, dividão, média aritimética e raíz quadrada.

--- 05_FirstStepsInJavawithSptingBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/MathController.java
+++ 05_FirstStepsInJavawithSptingBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/MathController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DecimalFormat;
 import java.util.Objects;
 
 @RestController
@@ -13,13 +14,59 @@ public class MathController {
 
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable(value = "numberOne") String numberOne,
-                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public String sum(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
 
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        double resultado = convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return formatDouble(resultado);
     }
 
-    private Double convertToDouble(String number) throws Exception {
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public String sub(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+
+        double resultado = convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return formatDouble(resultado);
+    }
+
+    private static String formatDouble(double resultado) {
+        return new DecimalFormat("#,##0.00").format(resultado);
+    }
+
+    @RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public String multiplicar(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+
+        double resultado = convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return formatDouble(resultado);
+    }
+
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public String div(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+    if("0".equals(numberTwo)){
+        throw new UnsuportedMathOperationException("Não é possível efetuar uma dividão por 0");
+    }
+        double resultado = convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return formatDouble(resultado);
+    }
+
+    @RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public String media(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+
+        double resultado = (convertToDouble(numberOne) + convertToDouble(numberTwo))/2;
+        return formatDouble(resultado);
+    }
+
+    @RequestMapping(value = "/raiz/{numberOne}", method = RequestMethod.GET)
+    public String raiz(@PathVariable(value = "numberOne") String numberOne) {
+
+        double resultado = Math.sqrt(convertToDouble(numberOne));
+        return formatDouble(resultado);
+    }
+
+    private Double convertToDouble(String number) {
         if (Objects.isNull(number)) {
             throw new NumberFormatException("O valor informado não é um número");
         }
