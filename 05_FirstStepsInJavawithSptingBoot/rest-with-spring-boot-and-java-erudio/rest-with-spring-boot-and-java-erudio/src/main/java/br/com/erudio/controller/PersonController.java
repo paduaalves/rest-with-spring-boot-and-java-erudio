6ead6d76701cdeb7bd45becddc1884440e6feff9
padua <paduaalves03@gmail.com>
2023-03-31 22:00:15 -0300

    Adicionando Content Negotiation

--- 05_FirstStepsInJavawithSptingBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
+++ 05_FirstStepsInJavawithSptingBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -1,9 +1,9 @@
 package br.com.erudio.controller;
 
 import br.com.erudio.service.PersonService;
+import br.com.erudio.util.MediaType;
 import br.com.erudio.vo.v1.PersonVO;
 import br.com.erudio.vo.v2.PersonVOV2;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,41 +19,41 @@ public class PersonController {
         this.personServices = personServices;
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public PersonVO findById(@PathVariable Long id) {
 
         return personServices.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public List<PersonVO> findByAll() {
 
         return personServices.findAll();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public PersonVO create(@RequestBody PersonVO vo) {
 
         return personServices.create(vo);
     }
 
-    @PostMapping(path = "/v2",consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/v2", consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public PersonVOV2 createV2(@RequestBody PersonVOV2 vo) {
 
         return personServices.createV2(vo);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public PersonVO update(@RequestBody PersonVO vo) {
 
         return personServices.update(vo);
     }
 
-    @DeleteMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML})
     public ResponseEntity<?> delete(@PathVariable Long id) {
         personServices.delete(id);
 
---
+++ 05_FirstStepsInJavawithSptingBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/util/MediaType.java
@@ -0,0 +1,10 @@
+package br.com.erudio.util;
+
+public class MediaType {
+
+    public final static String APPLICATION_JSON = "application/json";
+    public final static String APPLICATION_XML = "application/xml";
+    public final static String APPLICATION_YAML = "application/x-yaml";
+
+
+}
